e6ef005f87dab1f3b468b1cd2e4a45aec5ce5276
AndreiVelescu <160406656+AndreiVelescu@users.noreply.github.com>
2024-05-15 12:43:45 +0300

    Update employee.py

--- Lab 8/employee.py
+++ Lab 8/employee.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 
 class Employee:
@@ -10,37 +11,22 @@ class Employee:
         self.specialty = specialty
         self.monthly_salary = monthly_salary
 
-
     def calculateAge(self):
-        # implementați calculul vârstei aici
-        pass
+        today = datetime.now()
+        birth_date = datetime.strptime(self.birth_date, '%Y-%m-%d')
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+        return age
 
-    def calculateSalary(self):
-        # implementați calculul salariului aici
-        pass
-
-    # Getteri și setteri cu property() și decoratori
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if re.match("^[a-zA-Z ]+$", value):
-            self._name = value
-        else:
-            raise ValueError("Invalid name format")
-
-    # Alți getteri și setteri pentru celelalte proprietăți
+    
 
 
 class HourlyEmployee(Employee):
     def __init__(self, name, phone, birth_date, email, specialty, hourly_rate):
         super().__init__(name, phone, birth_date, email, specialty)
-        self._hourly_rate = hourly_rate
+        self.hourly_rate = hourly_rate
 
     def calculateSalary(self, hours_worked):
-        return self._hourly_rate * hours_worked
+        return self.hourly_rate * hours_worked
 
     # Alte metode specifice angajatului cu plată pe oră
 
@@ -48,9 +34,7 @@ class HourlyEmployee(Employee):
 class SalaryEmployee(Employee):
     def __init__(self, name, phone, birth_date, email, specialty, monthly_salary):
         super().__init__(name, phone, birth_date, email, specialty)
-        self._monthly_salary = monthly_salary
+        self.monthly_salary = monthly_salary
 
     def calculateSalary(self):
-        return self._monthly_salary
-
-    # Alte metode specifice angajatului cu salariu fix
+        return self.monthly_salary
